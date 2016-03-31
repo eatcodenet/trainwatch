76fca1cc66ec63e84354092185b6426cf3398e51
Ayub Malik <ayub.malik@gmail.com>
2016-03-31 22:01:26 +0100

    update compose

--- train-movement/src/main/java/net/eatcode/trainwatch/movement/kafka/KryoInstances.java
+++ train-movement/src/main/java/net/eatcode/trainwatch/movement/kafka/KryoInstances.java
@@ -1,4 +1,4 @@
-package net.eatcode.trainwatch.nr.hazelcast;
+package net.eatcode.trainwatch.movement.kafka;
 
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.pool.KryoFactory;
--- train-movement/src/main/java/net/eatcode/trainwatch/movement/kafka/TrainMovementProducer.java
+++ train-movement/src/main/java/net/eatcode/trainwatch/movement/kafka/TrainMovementProducer.java
@@ -1,37 +1,43 @@
 package net.eatcode.trainwatch.movement.kafka;
 
 import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Output;
-import net.eatcode.trainwatch.nr.Crs;
+import net.eatcode.trainwatch.movement.TrainMovementStompSubscription;
+import net.eatcode.trainwatch.movement.TrustTrainMovement;
+import net.eatcode.trainwatch.movement.TrustTrainMovementListener;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
 public class TrainMovementProducer {
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        Kryo kryo = new Kryo();
-        kryo.register(Crs.class);
+    private final String topicName = "trust-train-movements";
+    private final Properties props;
+    private final KafkaProducer<String, byte[]> producer;
+    private final Kryo kryo = new Kryo();
 
-        Properties props = new KafkaPropertiesBuilder().producerProperties("hp-z400:9092");
-        KafkaProducer<String, byte[]> producer = new KafkaProducer<>(props);
-        for (int i = 0; i < 2; i++) {
-            String random = System.currentTimeMillis() + "";
-            Crs crs = new Crs();
-            crs.crs = "crs" + random;
-            crs.name = "name" + random;
-            crs.lat = "lat" + random;
-            crs.lon = "lon" + random;
-            Output output = new Output(1024, 2048);
-            kryo.writeObject(output, crs);
-            output.flush();
-            output.close();
-            Thread.sleep(10);
-            producer.send(new ProducerRecord<>("crs1", "key" + random, output.getBuffer()));
-        }
-        producer.close();
+    public TrainMovementProducer(String boostrapServers) {
+        this.props = new KafkaPropertiesBuilder().producerProperties(boostrapServers);
+        this.producer = new KafkaProducer<>(props);
+        kryo.register(TrustTrainMovement.class);
+    }
 
+    public void subscribeToMovements(String username, String password) {
+        TrainMovementStompSubscription stompSubscription = new TrainMovementStompSubscription(username, password);
+        stompSubscription.subscribe(new TrustTrainMovementListener() {
+            @Override
+            public void onTrainMovement(TrustTrainMovement tm) {
+                byte[] data = serializeToBytes(tm);
+                producer.send(new ProducerRecord<>("crs1", "key" + random, output.getBuffer()));
+            }
+        });
+    }
+
+    private byte[] serializeToBytes(TrustTrainMovement tm) {
+        kryo.
+    }
+
+    public void close() {
+        this.producer.close();
     }
 }
