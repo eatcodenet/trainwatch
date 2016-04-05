7c39653900a7de3c48d538404ba95d51a2287492
Ayub Malik <ayub.malik@gmail.com>
2016-04-05 21:19:36 +0100

    wip

--- train-reference-data/src/main/java/net/eatcode/trainwatch/nr/dataimport/ScheduleRepositoryPopulator.java
+++ train-reference-data/src/main/java/net/eatcode/trainwatch/nr/dataimport/ScheduleRepositoryPopulator.java
@@ -32,7 +32,7 @@ public class ScheduleRepositoryPopulator {
     private void generateDailySchedulesFromTrustSchedule(TrustSchedule ts) {
         List<DaySchedule> daySchedules = new TransformTrustSchedule(locationRepo).toDaySchedules(ts);
         daySchedules.forEach(schedule -> {
-            log.debug("{}", schedule.key());
+            log.debug("{} {}", schedule.id, schedule.key());
             scheduleRepo.put(schedule);
         });
     }
