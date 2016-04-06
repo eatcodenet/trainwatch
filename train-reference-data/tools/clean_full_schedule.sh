efaa13479dc7b28b2cf2daf396a04b4027f31fe8
Ayub Malik <ayub.malik@gmail.com>
2016-04-06 09:59:22 +0100

    wip

---
+++ train-reference-data/tools/clean_full_schedule.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+full_schedule_file=${1}
+
+if [ -z "${full_schedule_file}" ];then
+  echo "usage: ${BASH_SOURCE[0]} <schedule file>"
+  exit 1
+fi
+
+cleaned_file="${full_schedule_file}.cleaned"
+
+echo "Writing to file ${cleaned_file}"
+grep JsonScheduleV1 < ${full_schedule_file} | grep schedule_location > ${cleaned_file}
+echo "Done."
+
