142d45591a62b45effb479cf57cef265e5103279
Ayub Malik <ayub.malik@gmail.com>
2016-04-08 14:51:32 +0100

    aws

--- aws/codedeploy-create-deployment.sh
+++ aws/codedeploy-create-deployment.sh
@@ -4,7 +4,7 @@ date_now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 aws --profile eatcode deploy create-deployment \
   --application-name TrainWatch \
-  --s3-location bucket=eatcode-trainwatch-deploy,key=${bundle_name},bundleType=zip,eTag="9ac814e54b82b515a726e54ec686df5b-2" \
+  --s3-location bucket=eatcode-trainwatch-deploy,key=${bundle_name},bundleType=zip" \
   --deployment-group-name trainwatch-deploy \
   --deployment-config-name codeDeployDefault.OneAtATime \
   --description "Deployment at ${date_now}
