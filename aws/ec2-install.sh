1ab635581726aa79117a8064b03ccef9ddf5d67d
Ayub Malik <ayub.malik@gmail.com>
2016-04-08 17:05:33 +0100

    wip

--- aws/ec2-install.sh
+++ aws/ec2-install.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 base_dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+app_dir=/var/trainwatch
+data_dir=${app_dir}/data
 
 command=${1:-"oops"}
+case "${command}" in
+  application-stop)
+    echo "stop.."
+    ;;
 
-# fake script - wip
-touch "${command}.txt"
+  before-install)
+    echo "before...
+    mkdir -p ${data_dir}
+    chown ec2-user ${data_dir}
+    ;;
 
+  after-install)
+    echo "after..."
+    ;;
+
+  application-start)
+    echo "start..."
+    ;;
+
+  validate-service)
+    echo "validate..."
+    ;;
+
+  *)
+    echo $"Usage: $0 {application-stop|before-install|after-install|application-start|validate-service}"
+    exit 1
+esac
