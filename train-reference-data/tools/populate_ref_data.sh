a327fd443dde80dfeb53e98438f873ebb07e94ce
Ayub Malik <ayub.malik@gmail.com>
2016-04-09 16:23:57 +0100

    populate data script

---
+++ train-reference-data/tools/populate_ref_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+app_dir=/var/trainwatch
+jar_file=${app_dir}/libs/train-reference-data-1.0-SNAPSHOT.jar
+crs_file=${app_dir}/data/stations.json
+tiploc_file=${app_dir}/data/corpus_tiplocs
+schedule_file=${app_dir}/data/toc-full.cleaned
+uname=$(uname)
+
+if [ "${uname}" == "Darwin" ];then
+  jar_file=${base_dir}/../build/libs/train-reference-data-1.0-SNAPSHOT.jar
+fi
+
+java -cp ${jar_file} net.eatcode.trainwatch.nr.dataimport.PopulateLocationsApp \
+  ${crs_file} ${tiploc_file}
+
+if [ $? -ne 0 ];then
+  echo "Population of locations failed!"
+  exit 1
+fi
