7baeea2ef437c1a34abfb9883df4e8dc83898f97
Ayub Malik <ayub.malik@gmail.com>
2016-04-11 11:14:52 +0100

    wip

--- train-reference-data/tools/populate_ref_data.sh
+++ train-reference-data/tools/populate_ref_data.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 app_dir=/var/trainwatch
-jar_file=${app_dir}/libs/train-reference-data-1.0-SNAPSHOT.jar
+jar_file=${base_dir}/../build/libs/train-reference-data-1.0-SNAPSHOT.jar
 crs_file=${app_dir}/data/stations.json
 tiploc_file=${app_dir}/data/corpus_tiplocs
 schedule_file=${app_dir}/data/toc-full.cleaned
 uname=$(uname)
 
-if [ "${uname}" == "Darwin" ];then
-  jar_file=${base_dir}/../build/libs/train-reference-data-1.0-SNAPSHOT.jar
-fi
-
+# if [ "${uname}" == "Darwin" ];then
+#   jar_file=${base_dir}/../build/libs/train-reference-data-1.0-SNAPSHOT.jar
+# fi
+echo "jar_file is ${jar_file}"
 echo "Populating locations"
 java -cp ${jar_file} net.eatcode.trainwatch.nr.dataimport.PopulateLocationsApp \
   ${crs_file} ${tiploc_file}
