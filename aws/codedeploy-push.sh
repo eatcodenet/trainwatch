16bb7bba34b3bc175feaa4b62b2bec6e2b2a02d1
Ayub Malik <ayub.malik@gmail.com>
2016-04-12 10:11:47 +0100

    ignore hidden

--- aws/codedeploy-push.sh
+++ aws/codedeploy-push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 bundle_name=${1:-"LatestBundle.zip"}
-aws --profile eatcode deploy push --application-name TrainWatch --s3-location s3://eatcode-trainwatch-deploy/${bundle_name}
+aws --profile eatcode deploy push --application-name TrainWatch --ignore-hidden-files --s3-location s3://eatcode-trainwatch-deploy/${bundle_name}
