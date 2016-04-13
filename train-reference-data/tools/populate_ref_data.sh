030a2854ce581d8e907682ecebea6e83050f83ca
Ayub Malik <ayub.malik@gmail.com>
2016-04-13 14:38:20 +0100

    wip

--- train-reference-data/tools/populate_ref_data.sh
+++ train-reference-data/tools/populate_ref_data.sh
@@ -4,7 +4,7 @@ app_dir=/var/trainwatch
 jar_file=${app_dir}/libs/train-reference-data-1.0-SNAPSHOT.jar
 crs_file=${app_dir}/data/stations.json
 tiploc_file=${app_dir}/data/tiplocs.json
-schedule_file=${app_dir}/data/toc-full.cleaned
+schedule_file=${app_dir}/data/full-train-schedules
 uname=$(uname)
 
 if [ "${uname}" == "Darwin" ];then
@@ -20,8 +20,8 @@ if [ $? -ne 0 ];then
   exit 1
 fi
 
-#echo "Populating schedules"
-#java -cp ${jar_file} net.eatcode.trainwatch.nr.dataimport.PopulateSchedulesApp ${schedule_file}
+echo "Populating schedules"
+java -cp ${jar_file} net.eatcode.trainwatch.nr.dataimport.PopulateSchedulesApp ${schedule_file}
 
 if [ $? -ne 0 ];then
   echo "Populating schedules failed!"
