129a80d7dbf50335c090c4a350f58face15798d6
Ayub Malik <ayub.malik@gmail.com>
2016-04-13 15:03:10 +0100

    wip

--- aws/codedeploy-create-deployment.sh
+++ aws/codedeploy-create-deployment.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+base_dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 bundle_name=${1:-"LatestBundle.zip"}
 date_now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
@@ -7,5 +9,5 @@ aws --profile eatcode deploy create-deployment \
   --s3-location bucket=eatcode-trainwatch-deploy,key=${bundle_name},bundleType=zip \
   --deployment-group-name trainwatch-deploy \
   --deployment-config-name CodeDeployDefault.OneAtATime \
-  --description "Deployment at ${date_now}"
+  --description "Trainwatch deployment at ${date_now}"
 
--- aws/codedeploy-push.sh
+++ aws/codedeploy-push.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+base_dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#${base_dir}/../gradlew clean build
+
+rm -rf ${base_dir}/train-reference-data/build/full-train-schedules
+rm -rf ${base_dir}/train-reference-data/build/tiplocs.json
+rm -rf ${base_dir}/train-reference-data/build/stations.json
+rm -rf ${base_dir}/train-reference-data/build/classes
+rm -rf ${base_dir}/train-reference-data/build/dependency-cache
+rm -rf ${base_dir}/train-reference-data/build/tmp
+
 bundle_name=${1:-"LatestBundle.zip"}
 aws --profile eatcode deploy push --application-name TrainWatch --ignore-hidden-files --s3-location s3://eatcode-trainwatch-deploy/${bundle_name}
