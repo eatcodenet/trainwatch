c4d35c574e36c797291da9cadaf74512b4a9ee76
Ayub Malik <ayub.malik@gmail.com>
2016-04-28 16:50:17 +0100

    wip

---
+++ train-reference-data/tools/download_all_ref_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+app_dir=/var/trainwatch
+creds=${app_dir}/creds.txt
+
+if [ ! -r ${creds} ]; then
+	echo "no readable creds. see ya!"
+	exit 1
+fi
+
+username=$(grep username ${creds} | cut -d'=' -f2)
+password=$(grep password ${creds} | cut -d'=' -f2)
+
+${base_dir}/download_full_schedule.sh ${username} ${password}
+${base_dir}/download_tiplocs.sh ${username} ${password}
+${base_dir}/download_stations.sh ${username} ${password}
