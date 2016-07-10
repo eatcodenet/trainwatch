6788784356c88d0cf94b0450a5e64f35bf0c0a3a
Ayub Malik <ayub.malik@gmail.com>
2016-07-10 20:46:40 +0100

    ip

--- tw-train-movement/src/main/java/net/eatcode/trainwatch/movement/ehcache/EhcacheSpike.java
+++ tw-train-movement/src/main/java/net/eatcode/trainwatch/movement/ehcache/EhcacheSpike.java
@@ -1,29 +1,39 @@
 package net.eatcode.trainwatch.movement.ehcache;
 
+import java.net.URI;
+
 import org.ehcache.Cache;
 import org.ehcache.CacheManager;
+import org.ehcache.PersistentCacheManager;
+import org.ehcache.clustered.client.config.builders.ClusteredResourcePoolBuilder;
+import org.ehcache.clustered.client.config.builders.ClusteringServiceConfigurationBuilder;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.MemoryUnit;
 
 public class EhcacheSpike {
 
     public static void main(String[] args) {
 
-        CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
-                .withCache("refdata",
+        CacheManagerBuilder<PersistentCacheManager> builder = CacheManagerBuilder.newCacheManagerBuilder()
+                .with(ClusteringServiceConfigurationBuilder
+                        .cluster(URI.create("terracotta://localhost:9510/trainwatch"))
+                        .autoCreate()
+                        .defaultServerResource("primary-server-resource"))
+                .withCache("cache2",
                         CacheConfigurationBuilder.newCacheConfigurationBuilder(Integer.class, String.class,
-                                ResourcePoolsBuilder.heap(10)))
-                .build(true);
+                                ResourcePoolsBuilder.newResourcePoolsBuilder()
+                                        .with(ClusteredResourcePoolBuilder.clusteredDedicated("primary-server-resource",
+                                                32, MemoryUnit.MB))));
 
-        Cache<Integer, String> refdata = cacheManager.getCache("refdata", Integer.class, String.class);
+        PersistentCacheManager cacheManager = builder.build(true);
 
-        refdata.put(1, "mummy");
-        refdata.put(2, "daddy");
-        
-        System.out.println(refdata.get(1));
-        System.out.println(refdata.get(2));
-        System.out.println(refdata.get(3));
+        Cache<Integer, String> cache = cacheManager.getCache("cache2", Integer.class, String.class);
+        cache.put(1, "alpha" + System.currentTimeMillis());
 
+        System.out.println(cache.get(1));
+        cacheManager.close();
+        System.out.println("DONE");
     }
 }
