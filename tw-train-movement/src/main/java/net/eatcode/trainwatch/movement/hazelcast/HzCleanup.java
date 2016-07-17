fe3057ff849e34bd44b23de527cec48025cf538a
Ayub Malik <ayub.malik@gmail.com>
2016-07-17 14:53:19 +0100

    more tidy up

--- tw-train-movement/src/main/java/net/eatcode/trainwatch/movement/hazelcast/HzCleanup.java
+++ tw-train-movement/src/main/java/net/eatcode/trainwatch/movement/hazelcast/HzCleanup.java
@@ -6,16 +6,17 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 
-import net.eatcode.trainwatch.movement.MovementRepo;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.eatcode.trainwatch.movement.MovementRepo;
 
 public class HzCleanup {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private final long interval = 60 * 3;
+    private final int ageInHours = 2;
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     private ScheduledFuture<?> task;
@@ -31,7 +32,7 @@ public class HzCleanup {
         Runnable evict = new Runnable() {
             @Override
             public void run() {
-                movementRepo.evictOlderThan(3);
+                movementRepo.evictOlderThan(ageInHours);
             }
         };
         task = scheduler.scheduleAtFixedRate(evict, interval, interval, SECONDS);
