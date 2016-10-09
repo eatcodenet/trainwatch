c847bb539ad6c08b628190e525e1126b6fe6acfe
Ayub Malik <ayub.malik@gmail.com>
2016-10-09 17:43:08 +0100

    USe rysnc

--- deploy/deploy-backend.sh
+++ deploy/deploy-backend.sh
@@ -16,12 +16,13 @@ fi
 ssh -o StrictHostKeyChecking=no ${host} "touch ${deploy_dir}/start.txt"
 ssh ${host} "mkdir -p ${deploy_dir}/{data,libs,../logs}"
 
-scp ${src_dir}/tw-ref-data/tools/*.sh ${host}:${deploy_dir}
-scp ${src_dir}/tw-ref-data/build/libs/*.jar ${host}:${deploy_dir}/libs
-scp ${src_dir}/tw-train-movement/build/libs/*.jar ${host}:${deploy_dir}/libs
-scp ${src_dir}/tw-search-api/build/libs/*.jar ${host}:${deploy_dir}/libs
-scp ${base_dir}/ref-data/*.json ${host}:${deploy_dir}/data
-
+echo "Rsync files..."
+rsync -az ${src_dir}/tw-ref-data/tools/ ${host}:${deploy_dir}/
+rsync -az ${src_dir}/tw-ref-data/build/libs/ ${host}:${deploy_dir}/libs/
+rsync -az ${src_dir}/tw-train-movement/build/libs/ ${host}:${deploy_dir}/libs/
+#rsync -az ${src_dir}/tw-search-api/build/libs/ ${host}:${deploy_dir}/libs/
+rsync -az ${base_dir}/ref-data/ ${host}:${data_dir}/
+rsync -az ${base_dir}/kafka/bin/ ${host}:${kafka_dir}/bin/
 
 if [[ -z "${nr_username}" || -z "${nr_password}" ]]; then
   echo "WARNING: set nr_username and nr_password to enable download of data"
@@ -38,19 +39,24 @@ if [[ -z "${schedule_file}" ]]; then
   ssh ${host} "/var/trainwatch/deploy/get-schedule-file-only.sh"
 fi
 
+# Change memory settings
+ssh ${host} "sed -i 's/^# MAX_HEAP_SIZE=1G/MAX_HEAP_SIZE=256M/g' ${hazelcast_dir}/bin/start.sh"
+ssh ${host} "sed -i 's/Xms512M/Xms256M/g; s/Xmx512M/Xmx256m/g' ${kafka_dir}/bin/zookeeper-server-start.sh"
+ssh ${host} "sed -i 's/Xms1G/Xms256M/g; s/Xmx1G/Xmx256m/g' ${kafka_dir}/bin/kafka-server-start.sh"
+
 echo "Stopping Kafka/Zookeeper/Hazelcast"
 ssh ${host} "${kafka_dir}/bin/kafka-server-stop.sh"
 ssh ${host} "${kafka_dir}/bin/zookeeper-server-stop.sh"
 ssh ${host} "${hazelcast_dir}/bin/stop.sh"
 
-echo "Starting Hazelcast/Zookeeper/Kafka"
-ssh ${host} "sed -i 's/^# MAX_HEAP_SIZE=1G/MAX_HEAP_SIZE=256M/g' ${hazelcast_dir}/bin/start.sh"
-ssh ${host} "sed -i 's/Xms512M/Xms256M/g; s/Xmx512M/Xmx256m/g' ${kafka_dir}/bin/zookeeper-server-start.sh"
-ssh ${host} "sed -i 's/Xms1G/Xms256M/g; s/Xmx1G/Xmx256m/g' ${kafka_dir}/bin/kafka-server-start.sh"
-ssh ${host} -f "nohup ${hazelcast_dir}/bin/start.sh >${logs_dir}/hazelcast.log"
+echo "Starting Zookeeper and waiting 5s"
 ssh ${host} -f "nohup ${kafka_dir}/bin/zookeeper-server-start.sh ${kafka_dir}/config/zookeeper.properties >${logs_dir}/zookeeper.log"
-# dirty hack
-sleep 1
+ssh ${host} "${kafka_dir}/bin/wait-for-it.sh localhost:2181 -s -t 5"
+
+echo "Starting Hazelcast/Kafka"
+ssh ${host} -f "nohup ${hazelcast_dir}/bin/start.sh >${logs_dir}/hazelcast.log"
+#hack
+sleep 5
 ssh ${host} -f "nohup ${kafka_dir}/bin/kafka-server-start.sh ${kafka_dir}/config/server.properties >${logs_dir}/kafka.log"
 
 echo "Deploy complete."
