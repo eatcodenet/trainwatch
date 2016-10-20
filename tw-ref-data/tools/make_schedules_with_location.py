442917fcdeb54ae41eb78f067d160e62f8872d0f
Ayub Malik <ayub.malik@gmail.com>
2016-10-20 19:59:49 +0100

    Add orig/dest to schedule

--- tw-ref-data/tools/make_schedules_with_location.py
+++ tw-ref-data/tools/make_schedules_with_location.py
@@ -1,10 +1,11 @@
 import json
 import time
-
+from itertools import izip
 
 def load_locations(data_file):
     with open(data_file, "r") as loc_file:
-        return json.load(loc_file)["locations"]
+        locations = json.load(loc_file)["locations"]
+        return {loc["tiploc"]:loc for loc in locations}
 
 
 def load_schedules(data_file):
@@ -23,19 +24,23 @@ def make_schedule(line):
     dest = next((l for l in locs if l["location_type"] == "LT"))
     start = s["schedule_start_date"]
     end = s["schedule_end_date"]
-    return {"id": id, "startDate": start, "endDate": end, "origin": orig,
-            "destination": dest}
+    return {"id": id, "startDate": start, "endDate": end, "origin": orig["tiploc_code"],
+            "destination": dest["tiploc_code"], "departure": orig["departure"], "arrival": dest["arrival"]}
 
-def write_to_file(schedules):
-    with open("schedules_with_locations.json", "w") as out:
+def write_to_file(schedules, locations):
+    with open("/var/trainwatch/data/schedules_with_locations.json", "w") as out:
         for s in schedules:
+            s["origin"] = locations[s["origin"]]
+            s["destination"] = locations[s["destination"]]
+            print(s)
             json.dump(s, out)
             out.write("\n")
 
 
-print(load_locations("locations.json"))
+locations = load_locations("/var/trainwatch/data/locations.json")
+
 start = time.time()
-schedules = load_schedules("schedules.json")
+schedules = load_schedules("/var/trainwatch/data/schedules.json")
 end = time.time() - start
 print("took {0:.2f}s".format(end))
-write_to_file(schedules)
+write_to_file(schedules, locations)
