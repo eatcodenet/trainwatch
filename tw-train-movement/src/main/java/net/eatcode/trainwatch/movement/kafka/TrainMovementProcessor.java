51c1be036c44550c6bf5166bc29c15b49a249504
Ayub Malik <ayub.malik@gmail.com>
2016-10-28 14:45:56 +0100

    remove check for is passenger

--- tw-train-movement/src/main/java/net/eatcode/trainwatch/movement/kafka/TrainMovementProcessor.java
+++ tw-train-movement/src/main/java/net/eatcode/trainwatch/movement/kafka/TrainMovementProcessor.java
@@ -12,55 +12,51 @@ import net.eatcode.trainwatch.movement.TrainMovement;
 
 public class TrainMovementProcessor implements Processor<String, TrainMovement> {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private final MovementRepo movementRepo;
+	private final MovementRepo movementRepo;
 
-    private final ActivationRepo activationRepo;
+	private final ActivationRepo activationRepo;
 
-    private final DeparturesRepo departuresRepo;
+	private final DeparturesRepo departuresRepo;
 
-    public TrainMovementProcessor(MovementRepo movementRepo, ActivationRepo activationRepo,
-            DeparturesRepo departuresRepo) {
-        this.movementRepo = movementRepo;
-        this.activationRepo = activationRepo;
-        this.departuresRepo = departuresRepo;
-    }
+	public TrainMovementProcessor(MovementRepo movementRepo, ActivationRepo activationRepo,
+			DeparturesRepo departuresRepo) {
+		this.movementRepo = movementRepo;
+		this.activationRepo = activationRepo;
+		this.departuresRepo = departuresRepo;
+	}
 
-    @Override
-    public void init(ProcessorContext context) {
-        log.info("context {}", context);
-    }
+	@Override
+	public void init(ProcessorContext context) {
+		log.info("context {}", context);
+	}
 
-    @Override
-    public void process(String key, TrainMovement tm) {
-        log.debug("Movement {}", tm);
-        if (tm.hasArrivedAtDest()) {
-            deleteArriveTrain(tm);
-        } else {
-            if (tm.isPassenger()) {
-                movementRepo.put(tm);
-                departuresRepo.delete(tm.trainId());
-            } else {
-                log.debug("Skipping non passenger train - {}", tm.trainId());
-            }
-        }
-    }
+	@Override
+	public void process(String key, TrainMovement tm) {
+		log.debug("Movement {}", tm);
+		if (tm.hasArrivedAtDest()) {
+			deleteArriveTrain(tm);
+		} else {
+			movementRepo.put(tm);
+			departuresRepo.delete(tm.trainId());
+		}
+	}
 
-    private void deleteArriveTrain(TrainMovement tm) {
-        log.debug("Deleting arrived train: {}, {} - {}", tm.trainId(), tm.originCrs(), tm.destCrs());
-        movementRepo.delete(tm);
-        activationRepo.delete(tm.trainId());
-    }
+	private void deleteArriveTrain(TrainMovement tm) {
+		log.debug("Deleting arrived train: {}, {} - {}", tm.trainId(), tm.originCrs(), tm.destCrs());
+		movementRepo.delete(tm);
+		activationRepo.delete(tm.trainId());
+	}
 
-    @Override
-    public void punctuate(long timestamp) {
-        log.info("punctuate: {}", timestamp);
-    }
+	@Override
+	public void punctuate(long timestamp) {
+		log.info("punctuate: {}", timestamp);
+	}
 
-    @Override
-    public void close() {
-        log.info("close");
-    }
+	@Override
+	public void close() {
+		log.info("close");
+	}
 
 }
